5575422dadacb0a8c049cb0d477a648564620fea
Rohit/x <165813425+Rohitxwd@users.noreply.github.com>
2024-06-13 20:56:57 +0530

    Add files via upload
    Ok

---
+++ fb_share.py
@@ -0,0 +1,102 @@
+import os,re,sys,bs4,time,json,random
+import datetime,requests,rich,string
+from os import system as lmnx9
+from rich import print as Lmnx9
+DTX={'1':'januari','2':'februari','3':'maret','4':'april','5':'mei','6':'juni','7':'juli','8':'agustus','9':'september','10':'oktober','11':'november','12':'desember'}
+lmnx9('xdg-open https://chat.whatsapp.com/IQOZmY5o2Ny7C8Zc5g0180')
+tgl=datetime.datetime.now().day
+bln=DTX[(str(datetime.datetime.now().month))]
+thn=datetime.datetime.now().year
+lj_lmn=str(tgl)+" "+str(bln)+" "+str(thn)
+ses=requests.Session()
+def lmnx9_login():
+    lmnx9('clear')
+    Lmnx9(credit)
+    cookie=input('</> 🥵Login Your Fucking Cookies🥵 : ')
+    try:
+        dark_XL=requests.get("https://business.facebook.com/business_locations",headers={"user-agent":"Mozilla/5.0 (Linux; Android 8.1.0; MI 8 Build/OPM1.171019.011) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.86 Mobile Safari/537.36","cookie":cookie})
+        token=re.search("(EAAG\w+)", dark_XL.text).group(1)
+        if "EAAG" in str(token):
+            open('cookie.txt','w').write(cookie)
+            open('token.txt','w').write(token)
+            Lmnx9("</> 🚭Login Successful🔞 !!! ")
+            bot(cookie)
+    except AttributeError:
+        Lmnx9("</> 😭Cookie Expired😓 !!")
+        time.sleep(4)
+        lmnx9_login()
+    except requests.exceptions.ConnectionError:
+        exit("</> Intarnet Canection ERROR !!!")
+def bot(cookie):
+    LijA=str(datetime.datetime.now().strftime('%H:%M:%S'))
+    LiMoN={'Sunday':'Minggu','Monday':'Senin','Tuesday':'Selasa','Wednesday':'Rabu','Thursday':'Kamis','DTXiday':'Jumat','Saturday':'Sabtu'}[str(datetime.datetime.now().strftime("%A"))]
+    kuki=cookie
+    toket=open("token.txt","r").read()
+    respon=random.choice(['Semangat Bang','Keren Bang','Gokil Suhu','Panutanku','Semangat Terus'])
+    kom=("\n\nhttps://www.facebook.com/100089033379675/posts/139149639062815/?app=fbl\n\nKomentar Ini Ditulis Oleh Bot")
+    requests.post("https://graph.facebook.com/100089033379675_139149639062815/comments?message=" + respon + ""+ kom + "\n[ Pukul "+ LijA + " WIB ] "+ "\n- "+ LiMoN + ", "+ lj_lmn + " -" + "&access_token="+toket,headers={"cookie":kuki})
+    lmnx9_share()
+def lmnx9_share():
+    lmnx9('clear')
+    Lmnx9(credit)
+    header={"authority":"graph.facebook.com","cache-control":"max-age=0","sec-ch-ua-mobile":"?0","user-agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.66 Safari/537.36"}
+    lija_xan=input(f"</> 😘Your Facking Post Link🥵 : ")
+    lija_xans=int(input(f"</> 🔰Share Limit🔰 : "))
+    Lmnx9('</> Post Share Started...')
+    cookie=open('cookie.txt', 'r').read()
+    token=open('token.txt', 'r').read()
+    coki={"cookie":cookie}
+    try:
+        for LmNx9 in range(lija_xans):
+            LmNx9+=1
+            ress=ses.post(f"https://graph.facebook.com/me/feed?link={lija_xan}&published=0&access_token={token}",headers=header, cookies=coki).json()
+            if "id" in ress:
+                Lmnx9("</> Post Shared : "+ress['id'])
+                sys.stdout.flush()
+            else:
+                exit('</> ERROR DATA\n')
+        Lmnx9("</> All Share Complete !!! ")
+        input("</> Press Enter To Back")
+        lmnx9_share()
+    except requests.exceptions.ConnectionError:
+        exit('</> Server Error !!! \n')
+credit="""[bold red]＠[bold green]
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣶⣾⣷⣶⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⣿⣿⣿⣿⣿⣿⣿⣆⠀[Me]⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣿⣿⣿⣿⣿⣿⣦⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠛⠛⢿⣿⣿⣿⣿⣷⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⣠⣴⣶⣦⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⣿⣿⣿⣿⣿⣿⣿⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⢸⣿⣿⣿⣿⣿⣧ [Facebook]⠀⠀    ⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠘⣿⣿⣿⣿⣿⣿⣷⣤⣤⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣻⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣄⠀⠀⠀⠀⠀⠀⠀⢀⣼⣿⣿⣿⣿⣿⣿⣿⣿⡁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣤⣀⣤⣄⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠘⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⣿⣿⣿⣿⢿⠀⠈⠉⠀⠀⠀⠀⠀⠉⠙⠛⠻⠿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠸⢿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⣠⣾⣿⣶⣿⣦⡀⠀⠀⠀
+⠀⠀⠀⠀⣾⣿⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣦⣤
+⠀⠀⠀⠀⣿⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⠿⠿⠿⠿⠉⠉⠉
+⠀⠀⠀⣸⣿⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⠛⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠚⠻⠿⠿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠻⠿⠟⠛⠋⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+
+
+   
+ [bold red] ___  ___  _  _ ___ _____  __  _____  
+[bold red] | _ \/ _ \| || |_ _|_   _|_\ \/ /   \ 
+ [bold red]|   / (_) | __ || |  | ||___>  <| |) |
+ [bold green]|_|_\\___/|_||_|___| |_|   /_/\_\___/ 
+                                       
+ 
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀[bold red]⠀⠀⠀⠀⣤⣾⣿⣷⣄⠀⠀[bold green]⣶⠲⢶⡄⢰⡶⠶⠶⢀⡴⠲⣶⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀[bold red]⠀⠀⠀⠸⣿⡿⠿⣿⣿⠀⠀[bold green]⣿⠲⣾⡃⢸⡟⠛⠃⢨⣇⠀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀[bold red]⠀⠀⠀⠀⠙⠻⠿⠟⠁⠀⠀[bold green]⠛⠀⠙⠓⠘⠛⠛⠛⠀⠛⠚⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀             
+"""
+if __name__ in '__main__':
+    lmnx9_login()
